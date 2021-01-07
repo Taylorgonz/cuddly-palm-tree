22c57df9bdcbbeac893abed68aa613f7704a47b1
Taylor Gonzales <hello@taylorgonz.com>
2021-01-06 19:11:55 -0500

    activity 12 completed

--- 04-Web-APIs/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -8,5 +8,14 @@ function setCounterText() {
 }
 
 // TODO: Add event listener to increment button
-
+  incrementEl.addEventListener("click", ()=> {
+    count ++;
+    setCounterText();
+  }); 
 // TODO: Add event listener to decrement button 
+  decrementEl.addEventListener("click", ()=> {
+    if (count > 0) {
+    count --;
+    setCounterText();
+    }
+  });
