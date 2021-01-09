aee5405c1cf3d132d5ebc5eb343c25361bff9121
Taylor Gonzales <hello@taylorgonz.com>
2021-01-09 10:41:21 -0500

    activity 22 complete

--- 04-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -5,8 +5,6 @@ var msgDiv = document.querySelector("#msg");
 var userEmailSpan = document.querySelector("#user-email");
 var userPasswordSpan = document.querySelector("#user-password");
 
-renderLastRegistered();
-
 function displayMessage(type, message) {
   msgDiv.textContent = message;
   msgDiv.setAttribute("class", type);
@@ -14,6 +12,14 @@ function displayMessage(type, message) {
 
 function renderLastRegistered() {
   // TODO: Retrieve the last email and password and render it to the page
+  let email = localStorage.getItem("email");
+  let password = localStorage.getItem("password");
+  
+  if (!email || !password) {
+    return;
+  }
+  userEmailSpan.textContent = email;
+  userPasswordSpan.textContent = password;
 }
 
 signUpButton.addEventListener("click", function(event) {
@@ -30,5 +36,8 @@ signUpButton.addEventListener("click", function(event) {
     displayMessage("success", "Registered successfully");
 
   // TODO: Save email and password to localStorage and render the last registered user
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+  renderLastRegistered();
   }
 });
